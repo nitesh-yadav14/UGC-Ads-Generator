77f7f6496defb24e65430bf873bc44f4fe6240da
Nitesh Yadav <yadav.nitesh1009@gmail.com>
2026-04-29 15:21:16 +0530

    Update axios.ts

--- client/src/configs/axios.ts
+++ client/src/configs/axios.ts
@@ -1,9 +1,7 @@
-import axios from "axios"
+import axios from "axios";
 
 const api = axios.create({
-baseURL:
-import.meta.env.VITE_API_URL ||
-"https://ugc-ads-generator-backend.onrender.com",
-})
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000",
+});
 
-export default api
+export default api;
