a4179d61dc7fe20d64e4d275ede32397040c362b
Nitesh Yadav <yadav.nitesh1009@gmail.com>
2026-04-29 13:48:17 +0530

    Update index.ts

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,36 +1,40 @@
 import type React from "react";
 
-
-
 export interface UploadZoneProps {
-  label : String;
-  file : File | null;
-  onClear : ()=> void;
-  onChange : (e: React.ChangeEvent<HTMLInputElement>)=> void;
+label: string;
+file: File | null;
+onClear: () => void;
+onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export interface User{
-  id? : string;
-  name? : string;
-  email? : string;
+export interface User {
+id?: string;
+name?: string;
+email?: string;
 }
 
-export interface Projects{
-  id:string;
-  name? : string;
-  userId? : String;
-  user? :User;
-  productName: string;
-  productDescription ? : string;
-  userPrompt ?: string;
-  aspectRatio : string;
-  targetLength ?: number;
-  generatedImage ?: string;
-  generatedVideo ?: string;
-  isGenerating: boolean;
-  isPublished: boolean;
-  error ?: string;
-  createdAt: Date | string;
-  updatedAt ?: Date | string;
-  uploadedImage: string[];
+export interface Project {
+id: string;
+name?: string;
+userId?: string;
+user?: User;
+
+productName: string;
+productDescription?: string;
+userPrompt?: string;
+
+aspectRatio: string;
+targetLength?: number;
+
+generatedImage?: string;
+generatedVideo?: string;
+
+isGenerating: boolean;
+isPublished: boolean;
+error?: string;
+
+createdAt: Date | string;
+updatedAt?: Date | string;
+
+uploadedImage: string[];
 }
